e4d593a3c5a5d936d1d1646f014ab503f6ae8d8a
Gabriel Isotton <isotton_gabriel@live.com>
2022-05-06 17:18:56 -0300

    ajustes backend deploy 05

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "ajustes backend deploy 04"
+git commit -m "ajustes backend deploy 05"
 git branch -M master
 git push -u origin master
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log("HTTP server is running " + (process.env.PORT || 3333));
+  console.log(`HTTP server is running ${process.env.PORT || 3333}`);
 });
